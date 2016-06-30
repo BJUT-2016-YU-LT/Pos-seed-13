0e71029798b23e681db86378a8818e110ba61ac3
fengshuoshuo <690849351@qq.com>
2016-06-30 21:10:28 +0800

    requirement4test
    商品编号请规定输入格式，输入格式错误请提示重新输入；折扣未设置输入错误提示；录入商品编号时未设置抛出异常（已在注释中标识）

---
+++ requirement4test/src/ruangong2/Main.java
@@ -0,0 +1,178 @@
+package ruangong2;
+import java.util.Scanner;
+class Main
+{
+    public static void main(String[] args)
+    {
+        String name;
+        String unit;
+        String barcode = null;
+        String pricee;
+        float price;
+        float discount;
+        int code;
+        int i;
+        int[] h=new int[1000000];
+        int[] c=new int[1000000];
+        for(i=0;i<1000000;i++)
+        {
+            h[i]=0;
+        }
+        goods[] a=new goods[1000000];
+        int j=0;
+        while(true)
+        {
+            if(j==1) {
+                barcode = "ITEM000000";
+                name = "可口可乐";
+                unit = "瓶";
+                pricee = "3.00";
+                discount=-1;
+            }
+            else if(j==0)
+            {
+                barcode="ITEM000001";
+                name="雪碧";
+                unit="瓶";
+                pricee="3.00";
+                discount=(float) 0.8;
+            }
+            else
+            {
+                barcode="ITEM000004";
+                name="电池";
+                unit="个";
+                pricee="2.00";
+                discount=(float)0.5;
+            }
+            code=(((int)(barcode.charAt(9)))-48)
+                    +(((int)(barcode.charAt(8)))-48)*10
+                    +(((int)(barcode.charAt(7)))-48)*100
+                    +(((int)(barcode.charAt(6)))-48)*1000
+                    +(((int)(barcode.charAt(5)))-48)*10000
+                    +(((int)(barcode.charAt(4)))-48)*100000;
+            /*if(h[code]==0)
+            {*/
+            price=(float)(((float)((int)((pricee.charAt(3))-48)))*0.01)
+                    +(float)(((float)((int)((pricee.charAt(2))-48)))*0.1)
+                    +(float)((int)((pricee.charAt(0))-48));
+            a[code]=new goods(barcode,name,unit,price,discount);
+            	/*h[code]++;
+            }
+            else
+            {
+            	h[code]++;
+            }*/
+            if(j==2)
+            {
+                break;
+            }
+            j++;
+        }
+        while(true)
+        {
+            if(j<7)
+            {
+                barcode="ITEM000000";
+            }
+            else if(j==7)
+            {
+                barcode="ITEM000001";
+            }
+            else if(j==8)
+            {
+                barcode="ITEM000001";
+            }
+            else
+            {
+                barcode="ITEM000004";
+            }
+            code=(((int)(barcode.charAt(9)))-48)+(((int)(barcode.charAt(8)))-48)*10+(((int)(barcode.charAt(7)))-48)*100+(((int)(barcode.charAt(6)))-48)*1000+(((int)(barcode.charAt(5)))-48)*10000+(((int)(barcode.charAt(4)))-48)*100000;
+            h[code]++;
+            if(j==9)
+            {
+                break;
+            }
+            j++;
+        }
+        System.out.println("***商店购物清单***");
+        float total=0;
+        float save=0;
+        float totalcount=0;
+        float count;
+        for(i=0;i<1000000;i++)
+        {
+            if(h[i]>0)
+            {
+                if(a[i].Getdiscount()==-1)
+                {
+                    count=(float)(a[i].Getprice()*(int)(h[i]/3));
+                    System.out.printf("名称:%s,数量:%d%s,单价:%.2f(元),小计:%.2f(元)\n",a[i].Getname(),h[i],a[i].Getunit(),a[i].Getprice(),a[i].Getprice()*(h[i]-h[i]/3));
+                    total+=(float)(a[i].Getprice()*(h[i]-h[i]/3));
+                    totalcount+=count;
+                    c[i]=h[i]/3;
+                }
+                else
+                {
+                    System.out.printf("名称:%s,数量:%d%s,单价:%.2f(元),小计:%.2f(元)\n",a[i].Getname(),h[i],a[i].Getunit(),a[i].Getprice(),a[i].Getprice()*h[i]*a[i].Getdiscount());
+                    total+=(float)(a[i].Getprice()*h[i]*a[i].Getdiscount());
+                    save+=(float)(a[i].Getprice()*h[i]*(1-a[i].Getdiscount()));
+                }
+            }
+        }
+        /*System.out.printf("名称:可口可乐,数量:%d,单价:3.00(元),小计:%d\n",c0,c0*3);
+        System.out.printf("名称:雪碧,数量:%d,单价:3.00(元),小计:%d\n",c1,c1*3);
+        System.out.printf("名称:电池,数量:%d,单价:2.00(元),小计:%d\n",c2,c2*2);*/
+        System.out.printf("----------------\n");
+        System.out.printf("挥泪赠送商品：\n");
+        for(i=0;i<1000000;i++)
+        {
+            if(c[i]>0)
+            {
+                System.out.printf("名称:%s,数量:%d%s\n",a[i].Getname(),c[i],a[i].Getname());
+            }
+        }
+        System.out.printf("----------------\n");
+        System.out.printf("总计:%.2f(元)\n",total);
+        System.out.printf("节省:%.2f(元)\n",save+totalcount);
+        System.out.printf("********************\n");
+    }
+    public static class goods
+    {
+        private String barcode;
+        private String name;
+        private String unit;
+        private float price;
+        private float discount;
+        goods(String a,String b,String c,float d,float e)
+        {
+            barcode=a;
+            name=b;
+            unit=c;
+            price=d;
+            discount=e;
+        }
+        public String Getbarcode()
+        {
+            return barcode;
+        }
+        public String Getname()
+        {
+            return name;
+        }
+        public String Getunit()
+        {
+            return unit;
+        }
+        public float Getprice()
+        {
+            return price;
+        }
+        public float Getdiscount()
+        {
+            return discount;
+        }
+    }
+
+}
+
